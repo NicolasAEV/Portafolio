d81bef59982ecf59e53d345028fdda52bd7ed2fe
NicolasAEV <NicolasEscobar.v@hotmail.com>
2022-12-22 22:02:24 -0300

    correccion a imagenes no visibles

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,63 +5,63 @@ const carritos = [{
     nombre: 'Asassins Creed Valhalla',
     descripcion: 'Juego de rol y aventura en el mundo vikingo',
     precio: 20000,
-    imagen: '/assets/img/valhalla.jpg'
+    imagen: './assets/img/valhalla.jpg'
 }, {
     Id : 2,
     cantidad : 1,
     nombre: 'Gears Of War',
     descripcion: 'Nvidia 4090',
     precio: 1500,
-    imagen: '/assets/img/gears.jpg'
+    imagen: './assets/img/gears.jpg'
 }, {
     Id : 3,
     cantidad : 1,
     nombre: 'God of war',
     descripcion: 'Nvidia 4090',
     precio: 1300,
-    imagen: '/assets/img/godofwar.jpg'
+    imagen: './assets/img/godofwar.jpg'
 }, {
     Id : 4,
     cantidad : 1,
     nombre: 'Ghost Recon Breackpoint',
     descripcion: 'Nvidia 4090',
     precio: 1000,
-    imagen: '/assets/img/ghost.jpg'
+    imagen: './assets/img/ghost.jpg'
 }, {
     Id : 5,
     cantidad : 1,
     nombre: 'Raimbow Six',
     descripcion: 'Nvidia 4090',
     precio: 800,
-    imagen: '/assets/img/raimbow.jpg'
+    imagen: './assets/img/raimbow.jpg'
 }, {
     Id : 6,
     cantidad : 1,
     nombre: 'Call of duty mw2',
     descripcion: 'Nvidia 4090',
     precio: 600,
-    imagen: '/assets/img/codmw2.jpg.jpg'
+    imagen: './assets/img/codmw2.jpg.jpg'
 }, {
     Id : 7,
     cantidad : 1,
     nombre: 'Elden Ring',
     descripcion: 'Nvidia 4090',
     precio: 450,
-    imagen: '3050.jpg'
+    imagen: './assets/img/elden.jpg'
 }, {
     Id : 8,
     cantidad : 1,
     nombre: 'Halo 5',
     descripcion: 'Nvidia 4090',
     precio: 400,
-    imagen: '/assets/img/halo5.jpg'
+    imagen: './assets/img/halo5.jpg'
 }, {
     Id : 9,
     cantidad : 1,
     nombre: 'GTA: Grand teft auto V',
     descripcion: 'Nvidia 4090',
     precio: 400,
-    imagen: '/assets/img/gtav.jpg'
+    imagen: './assets/img/gtav.jpg'
 }, {
     Id : 10,
     cantidad : 1,
